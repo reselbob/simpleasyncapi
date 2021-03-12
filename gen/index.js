d92133df38527b9c1c2bea20ec1a3cc4b92e7e0c
Bob Reselman <bob@CogArtTech.com>
2021-03-11 16:00:49 -0800

    adjusted file system

---
+++ gen/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const Generator = require('@asyncapi/generator');
+const generator = new Generator('@asyncapi/html-template', path.resolve(__dirname, 'example'));
+
+const runner = async () =>{
+    try {
+        await generator.generateFromURL('https://raw.githubusercontent.com/asyncapi/asyncapi/2.0.0/examples/2.0.0/streetlights.yml');
+        console.log('Done!');
+      } catch (e) {
+        console.error(e);
+      }
+}
+
+runner();
+
