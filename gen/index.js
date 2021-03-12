f5f21cd2fb04c3664aa52fd49e7b5d1733df1994
Bob Reselman <bob@CogArtTech.com>
2021-03-11 16:31:46 -0800

    working gen

--- gen/index.js
+++ gen/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Generator = require('@asyncapi/generator');
 const { AsyncAPIDocument } = require('@asyncapi/parser');
-const generator = new Generator('@asyncapi/nodejs-template', path.resolve(__dirname, 'example'));
+const generator = new Generator('@asyncapi/nodejs-template', path.resolve(__dirname, "./"));
 generator.templateParams = {server:"mosquitto"};
     generator
   .generateFromFile('asyncapi.yaml')
