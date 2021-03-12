7cc3b43eecb056982070e0c6f1e89ef40629d409
Bob Reselman <bob@CogArtTech.com>
2021-03-12 10:04:00 -0800

    updating gen code

--- gen/index.js
+++ gen/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const Generator = require('@asyncapi/generator');
-const generator = new Generator('@asyncapi/nodejs-template', path.resolve(__dirname, "output"));
+const generator = new Generator('@asyncapi/nodejs-template', path.resolve(__dirname, "../output"));
 generator.templateParams = {server:"mosquitto"};
     generator
   .generateFromFile('asyncapi.yaml')
